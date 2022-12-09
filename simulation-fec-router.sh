087b454c363a341f5cce5647a42e40d1ae6f8eea
jam-sasebo <s1844@st.sasebo.ac.jp>
2022-12-09 15:10:34 +0900

    updated .gitignore

---
+++ simulation-fec-router.sh
@@ -0,0 +1,73 @@
+#!/bin/bash 
+
+#実行したあとにちゃんとディレクトリが作られたかどうかを確認！！！！！！！！！！
+
+NOW=$( date +"%F" )
+dir="${NOW}/fec-router/"
+echo ${dir}
+
+for RngSeed in 5; do
+    for nRouters in 5; do
+        for nFlows in 10; do 
+
+            if [ -f "./fec-router-f${nFlows}-throughput.dat" ]; then
+                rm -f fec-router-f"${nFlows}"-throughput.dat
+            fi
+
+            for coredelayvalue in 100; do #5 10 25 50 100
+                for nGroups in 3 1; do #1 3 5 7 9
+                    for fwin in 20 ; do #10 20 50 75 100
+
+                        #結果ディレクトリを作成する
+                        mkdir -p "${dir}"s${RngSeed}
+                        mkdir -p "${dir}"s${RngSeed}/f${nFlows}
+                        mkdir -p "${dir}"s${RngSeed}/f${nFlows}/d${coredelayvalue}
+                        mkdir -p "${dir}"s${RngSeed}/f${nFlows}/d${coredelayvalue}/g${nGroups}
+                        mkdir -p "${dir}"s${RngSeed}/f${nFlows}/d${coredelayvalue}/g${nGroups}/w${fwin}
+                        DIR="${dir}s${RngSeed}/f${nFlows}/d${coredelayvalue}/g${nGroups}/w${fwin}/"
+                        
+                        #シミュレーションを実行する
+                        echo "./waf --run \"scratch/fec-router.cc --RngSeed=${RngSeed} --nRouters=${nRouters} --nFlows=${nFlows} --coredelay=${coredelayvalue} --fwin=${fwin} --nGroups=${nGroups}\" "
+                        ./waf --run "scratch/fec-router-3.cc --RngSeed=${RngSeed} --nRouters=${nRouters} --nFlows=${nFlows} --coredelay=${coredelayvalue} --fwin=${fwin} --nGroups=${nGroups}" 
+                        notify-send "f${nFlows}のシミュレーション終わった。結果処理をお願いします。"
+
+                        #cwndをプロットする
+                        ./cwndplot.sh fec-router
+                        mv fec-router-cwnd.eps "${dir}"fec-router-f"${nFlows}"-d"${coredelayvalue}"-g"${nGroups}"-w"${fwin}"-cwnd.eps
+                        
+                        #スループットを計算する
+                        echo "fec-router flow: ${nFlows} routers: ${nRouters} delay: ${coredelayvalue} group: ${nGroups} fwin: ${fwin}"
+                        sumthp=$(tail -n ${nFlows} FecRouter.TcpDump | awk -v f=${nFlows} -F' ' '{
+                            sumthp += ( ($9-1) * 8 ) / ( 220 * 1000 * 1000 );
+                        }END{
+                            print sprintf("%0.2f",sumthp);
+                        }')
+                        echo f ${nFlows} r ${nRouters} d ${coredelayvalue} g ${nGroups} w ${fwin} thp ${sumthp}
+                        echo f ${nFlows} r ${nRouters} d ${coredelayvalue} g ${nGroups} w ${fwin} thp ${sumthp} >> fec-router-f"${nFlows}"-throughput.dat
+                        sumthp=0
+
+
+                        #結果ファイルを移動
+                        mv FecRouter.TcpDump "${DIR}".
+                        mv FecRouter.Timeout "${DIR}".
+                        mv FecRouter.FastRecovery "${DIR}".
+                        mv FecRouter.Retransmit "${DIR}".
+                        mv FecRouter.LostPacket "${DIR}".
+                        mv FecRouter.SentPacket "${DIR}".
+                        mv FecRouter.Recovery "${DIR}".
+                        mv FecRouter.RoundFecTable "${DIR}".
+                        mv FecRouter.GroupFecTable "${DIR}".
+                        for i in $(seq 0 ${nFlows})
+                        do
+                            mv fec-router-cwnd"${i}".dat "${DIR}".
+                        done
+
+                    done
+                done
+            done
+        
+        mv fec-router-f"${nFlows}"-throughput.dat "${dir}".
+
+        done
+    done
+done
